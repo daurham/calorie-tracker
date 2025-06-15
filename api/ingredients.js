6ec838627ee47b595df2401e62af9c1c434464d6
Jake Daurham <daurham95@gmail.com>
2025-06-15 14:15:57 -0700

    converted file type to js

--- api/ingredients.js
+++ api/ingredients.js
@@ -1,7 +1,7 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
+// import { VercelRequest, VercelResponse } from '@vercel/node';
 import { sql } from '@vercel/postgres';
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+export default async function handler(req, res) {
   // Add CORS headers
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Origin', '*');
--- api/meal-combos.js
+++ api/meal-combos.js
@@ -1,7 +1,7 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
+// import { VercelRequest, VercelResponse } from '@vercel/node';
 import { sql } from '@vercel/postgres';
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+export default async function handler(req, res) {
   switch (req.method) {
     case 'GET':
       try {
